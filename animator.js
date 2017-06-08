063204c6c9b14702f222f1ab9e9c9f16cbe7c707
mage j lewis <mage.j.lewis@gmail.com>
2017-06-08 14:06:07 -0600

    Create animator.js

--- animator.js
+++ animator.js
@@ -3,9 +3,12 @@ var context = canvas.getContext("2d");
 var width = 32;
 var height = 32;
 var frames = [];
-for (var i; i<height; i++){
- 
+function newFrame(){
+frames[frames.length] = [];
+ for (var i; i<height; i++){
+  frames[frames.length-1][i] = [];
   for(var j; j<width; j++){
-    
+    frames[frames.length-1][i][j] = 0;
   }
+}
 }
