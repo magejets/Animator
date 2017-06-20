5ebafbd2c61a3a0bef9045e98bef23f872e64fee
mage j lewis <mage.j.lewis@gmail.com>
2017-06-20 11:56:53 -0600

    Update animator.js

--- animator.js
+++ animator.js
@@ -1,9 +1,12 @@
+//canvas setup
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+//constants
 var width = 32;
 var height = 32;
 var frames = [];
-function newFrame(){
+//loops through the array to make a 32 by 32 matrix
+function newFrame( width, height){
 frames[frames.length] = [];
  for (var i; i<height; i++){
   frames[frames.length-1][i] = [];
@@ -12,3 +15,4 @@ frames[frames.length] = [];
   }
 }
 }
+newFrame(width, height);
